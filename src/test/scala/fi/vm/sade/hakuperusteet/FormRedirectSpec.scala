cb9c910e7581fe586bb021145e11ca40e685dbef
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2015-12-07 15:23:36 +0200

    simplified unit test

--- src/test/scala/fi/vm/sade/hakuperusteet/FormRedirectSpec.scala
+++ src/test/scala/fi/vm/sade/hakuperusteet/FormRedirectSpec.scala
@@ -15,11 +15,13 @@ import org.scalatra.test.scalatest.ScalatraSuite
 class FormRedirectSpec extends FunSuite with ScalatraSuite with ServletTestDependencies {
 
   val formRedirect = new FormRedirectServlet(config, database, oppijanTunnistus, verifier, signer, countries, Mockito.mock(classOf[Tarjonta]))
-
-  val userOid: Some[Oid] = Some("person1")
-  val testUser = User(None, userOid, "test@test.com", Some("firstName"), Some("lastName"),
+  val rnd_number = scala.util.Random.nextInt(10000000)
+  val email = s"test_$rnd_number@test.com"
+  val userOid: Some[Oid] = Some(s"person_$rnd_number")
+  val testUser = User(None, userOid, email, Some("firstName"), Some("lastName"),
     Some(new Date(100)), None, IDPEntityId.google, Some("1"), Some("1"), Some("fi"), "fi")
   database.upsertUser(testUser)
+
   val appObject = ApplicationObject(None, userOid.get, "hakukohdeOid", "hakuOid", "educationLevel", "fi")
   val appSystem = ApplicationSystem("hakuOid", Some("formUri"), true, List())
 
