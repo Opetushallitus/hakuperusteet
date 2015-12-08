01538b3659df3d86621e8a658f29a6dfa5ffa320
Jani Rahkola <jani.rahkola@reaktor.com>
2015-12-08 18:41:23 +0200

    Clean DB before test

--- src/test/scala/fi/vm/sade/hakuperusteet/SynchronizationSpec.scala
+++ src/test/scala/fi/vm/sade/hakuperusteet/SynchronizationSpec.scala
@@ -15,7 +15,7 @@ import org.json4s.native.JsonMethods._
 import org.junit.runner.RunWith
 import org.mockito.{Mockito, Mock}
 import org.mockito.Matchers._
-import org.scalatest.FunSuite
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
 import org.scalatest.junit.JUnitRunner
 import org.scalatra.test.scalatest.ScalatraSuite
 import fi.vm.sade.hakuperusteet.domain.User._
@@ -23,7 +23,7 @@ import fi.vm.sade.hakuperusteet.domain.User._
 import scalaz.Failure
 
 @RunWith(classOf[JUnitRunner])
-class SynchronizationSpec extends FunSuite with ScalatraSuite with ServletTestDependencies {
+class SynchronizationSpec extends FunSuite with ScalatraSuite with ServletTestDependencies with BeforeAndAfterAll {
   val hakuAppMock = Mockito.mock(classOf[HakuAppClient])
   val tarjonta = Mockito.mock(classOf[Tarjonta])
   val synchronization = new Synchronization(config, database, tarjonta, countries, null) {
@@ -31,14 +31,16 @@ class SynchronizationSpec extends FunSuite with ScalatraSuite with ServletTestDe
     def publicCheckForId(id: Int) = checkSynchronizationForId(id)
   }
 
+  override def beforeAll() = {
+    HakuperusteetTestServer.cleanDB()
+  }
+
   test("haku-app synchronization") {
     val personOid = "4.4.4.4"
     val hakemusOid = "1.1.1.1"
     val email = "e@mail.com"
-    val user = database.findUser(email).orElse(database.upsertPartialUser(PartialUser(None, Some(personOid), email, IDPEntityId.oppijaToken, "en"))).get
-
-    val payment1 = Payment(None, personOid, new Date(), "1234", "1234", "1234", PaymentStatus.error,Some(hakemusOid))
-    database.upsertPayment(payment1)
+    val user = database.upsertPartialUser(PartialUser(None, Some(personOid), email, IDPEntityId.oppijaToken, "en")).get
+    val payment1 = database.upsertPayment(Payment(None, personOid, new Date(), "1234", "1234", "1234", PaymentStatus.error,Some(hakemusOid))).get
     val sync = database.insertPaymentSyncRequest(user, payment1).get
     Mockito.when(hakuAppMock.updateHakemusWithPaymentState(anyString(), any[PaymentState])).thenReturn(http4s.Response(Status.Forbidden))
     synchronization.publicCheckForId(sync.id)
