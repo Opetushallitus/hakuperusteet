3f6f6131edcf7c81c6ab410159b6c2d494b24fe2
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2016-09-06 16:27:48 +0300

    Log dbUrl

--- src/test/scala/fi/vm/sade/hakuperusteet/EmbeddedPostgreSql.scala
+++ src/test/scala/fi/vm/sade/hakuperusteet/EmbeddedPostgreSql.scala
@@ -1,9 +1,10 @@
 package fi.vm.sade.hakuperusteet
 
+import com.typesafe.scalalogging.LazyLogging
 import ru.yandex.qatools.embed.postgresql.{PostgresStarter}
 import ru.yandex.qatools.embed.postgresql.config.PostgresConfig
 
-object EmbeddedPostgreSql {
+object EmbeddedPostgreSql extends LazyLogging {
 
   private val runtime = PostgresStarter.getDefaultInstance()
   private val config = PostgresConfig.defaultWithDbName("test", "oph", "test")
@@ -12,7 +13,10 @@ object EmbeddedPostgreSql {
   private val dbUrl = s"jdbc:postgresql://${config.net().host()}:${config.net().port()}/${config.storage().dbName()}"
   private val user = s"${config.credentials().username()}"
   private val password = s"${config.credentials().password()}"
-  def configAsMap = Map("hakuperusteet.db.url"-> dbUrl, "hakuperusteet.db.user" -> user, "hakuperusteet.db.password" -> password)
+  def configAsMap = {
+    logger.info("Using embedded postgresql: " + dbUrl)
+    Map("hakuperusteet.db.url"-> dbUrl, "hakuperusteet.db.user" -> user, "hakuperusteet.db.password" -> password)
+  }
 
   def stop = {
     process.stop
