b5e1dbcdd0f26184b6d2e73e094431ea80954bc7
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2016-10-24 13:14:08 +0300

    Wait for postgres start

--- init_docker_postgres.sh
+++ init_docker_postgres.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 eval "$(docker-machine env dockerVM)"
 docker run -d -p 5432:5432 postgres
+while ! echo exit | nc hakuperusteetdb 5432; do sleep 10; done
 psql -hhakuperusteetdb -p5432 -Upostgres postgres -c "CREATE ROLE OPH;"
 psql -hhakuperusteetdb -p5432 -Upostgres postgres -c "CREATE DATABASE hakuperusteet;"
 psql -hhakuperusteetdb -p5432 -Upostgres postgres -c "CREATE DATABASE hakuperusteettest;"
