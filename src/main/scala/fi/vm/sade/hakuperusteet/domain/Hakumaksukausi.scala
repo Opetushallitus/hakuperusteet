f19fa76fca82c006c6e4466c5148aac835854c45
Tuukka Palomäki <tuukka.palomaki@gofore.com>
2017-01-16 14:23:49 +0200

    turhat pois

--- src/main/scala/fi/vm/sade/hakuperusteet/domain/Hakumaksukausi.scala
+++ src/main/scala/fi/vm/sade/hakuperusteet/domain/Hakumaksukausi.scala
@@ -1,23 +1,8 @@
 package fi.vm.sade.hakuperusteet.domain
 
-import java.util.Calendar
-
 object Hakumaksukausi extends Enumeration {
   type Hakumaksukausi = Value
   val s2016, k2017 = Value
-
-  def syncKoulutus(koulutuksenAlkamisVuosi: Option[Int]):Boolean = {
-    val alkamisVuosi: Int = (koulutuksenAlkamisVuosi match{
-      case Some(koulutuksenAlkamisVuosi:Int) => koulutuksenAlkamisVuosi
-      case _ => 3000
-    })
-
-    val year = Calendar.getInstance().get(Calendar.YEAR)
-    (year) match {
-      case year if year <= alkamisVuosi => true
-      case other => false
-    }
-  }
 
   def koulutuksenAlkamiskausiToHakumaksukausi(koulutuksenAlkamisVuosi: Option[Int], koulutuksenAlkamiskausiUri: Option[String]): Option[Hakumaksukausi] = {
     (koulutuksenAlkamisVuosi, koulutuksenAlkamiskausiUri) match {
