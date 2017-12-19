11dd0ae3cb4beeaeec33e3001719446fa2b50449
August Kilponen <august.kilponen@gmail.com>
2017-12-19 16:57:42 +0200

    Sähköpostiosoite pois footerista

--- src/main/assets/Footer.jsx
+++ src/main/assets/Footer.jsx
@@ -9,7 +9,6 @@ export default class Footer extends React.Component {
       <div>
         <div className="footer-logo">
           <a href={translation("footer.oph.link")} title={translation("footer.oph.title")}><img className="oph-logo" alt={translation("footer.oph.title")} src={"/hakuperusteet/img/OPH_logo-" + lang + ".svg"} /></a>
-          <div className="info"><a href="mailto:applicationfee@studyinfo.fi">applicationfee@studyinfo.fi</a></div>
         </div>
         <div className="footer-logo">
           <a href={translation("footer.okm.link")} title={translation("footer.okm.title")}><img alt={translation("footer.okm.title")} src={"/hakuperusteet/img/OKM_logo-" + lang + ".png"} /></a>
